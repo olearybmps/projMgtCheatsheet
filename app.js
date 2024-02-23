a9f8ee364546f7ac857a78b67ea1018f85fd87c4
olearybmps <160647100+olearybmps@users.noreply.github.com>
2024-02-22 21:22:28 -0500

    code for project management cheatsheet

---
+++ app.js
@@ -0,0 +1,6 @@
+// keyword --- name ---   () => {}
+const changeButton =  () => {
+    // this is where i put my instructions
+    let container = document.querySelector('.playContainer')
+   container.innerHTML=" HEY! THIS WORKS"
+}
